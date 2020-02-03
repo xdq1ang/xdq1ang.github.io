508f6048ca9c0d4844dac24a6163018cb4b741c4
e <1519445973@qq.com>
2020-02-03 23:39:41 +0800

    Site updated: 2020-02-03 23:39:41

--- dist/music.js
+++ dist/music.js
@@ -4,22 +4,19 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "lemon",
+        artist: '米津玄師',
+        url: 'http://stor.cloudmusics.cn/mp3/2019/12/e81d2ad553f302766c4806f99d688bc9.m4a',
+        cover: 'https://s2.ax1x.com/2020/02/03/10fMz4.jpg',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        name: '云烟成雨',
+        artist: '房东的猫',
+        url: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbmxkQzhUSldxS2tvV1ZmYnVra0VlWVFRQnFJP2U9d1Y3eVhE.mp3',
+        cover: 'https://s2.ax1x.com/2020/02/03/10f4yj.jpg',
       },
-      {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
-      }
+      
+	  
+	  
     ]
 });
